d76c278ac5d75a53bb081c7b30ba7a61599ec633
Luis Martinez <luis_mmartinez@live.com>
2021-01-06 05:32:04 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,32 @@
 class Customer
+  attr_accessor :name, :age
+  @@all = [ ]
   
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.customer == self}
+  end
+
+  def waiters
+    waiters_array= [ ]
+    Meal.all.each do |meal|
+      if meal.customer == self
+      waiters_array << meal.waiter
+      end
+    end
+    waiters_array
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,28 @@
 class Waiter
+  attr_accessor :name, :yearsofexperience
+  @all = [ ]
+
+  def initialize(name, yearsofexperience)
+    @name = name
+    @yearsofexperience = yearsofexperience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
+
+  def meals
+    Meal.all.select {|meal| meal.waiter == self}
+  end
+
+  def best_tipper
+    largest_tip= Meal.all.collect {|meal| meal.tip}.sort.last
+    largest_tip_customer= Meal.all.find {|meal| meal.tip == largest_tip}.customer
+  end
 
 end
